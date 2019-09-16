fb4c5ca8457dec9ae119edb8c3b3f678dcd82aed
James Quintero <jquinte2@mail.sfsu.edu>
2019-09-16 14:43:49 -0700

    Can display DICOM images and their masks denoting pneumothorax

--- DICOM_reader.py
+++ DICOM_reader.py
@@ -1,6 +1,9 @@
 import glob #for loading DICOM files from disk
 import pydicom #for reading DICOM files
 import matplotlib.pyplot as plt #for displaying DICOM files
+import pandas as pd #for reading data files
+
+from mask_functions import rle2mask
 
 
 images_path = './data/stage_2_images/*.dcm'
@@ -53,22 +56,85 @@ def plot_many_images():
         ax[q].imshow(dataset.pixel_array, cmap=plt.cm.bone)
 
 
+#adds mask to provided plot
+# def add_mask():
+
+
 def display_masks():
-    df = pd.read_csv('./data/train_labels.csv', header=None, index_col=0)
+    start = 0   # Starting index of images
+    num_img = 5 # Total number of images to show
 
-    print("Num training labels: "+str(df))
+    images_path = "./data/sample_train/*.dcm"
+    training_labels_path = "./data/train_labels.csv"
 
+
+    df = pd.read_csv(training_labels_path)
+
+    #prints the first 5 rows of each column separately
+    # print(df.head(5))
+
+    print("Num training labels: "+str(len(df)))
+
+    #creates charts/images
     fig, ax = plt.subplots(nrows=1, ncols=num_img, sharey=True, figsize=(num_img*10,10))
-    for q, file_path in enumerate(glob.glob('../input/siim-acr-pneumothorax-segmentation/sample images/*.dcm')[start:start+num_img]):
+
+    images = glob.glob(images_path)
+
+    for q, file_path in enumerate(images[start:start+num_img]):
         dataset = pydicom.dcmread(file_path)
-        #print(file_path.split('/')[-1][:-4])
+
+        # print(str(q)+": "+str(file_path))
+
+        #displays the body scan
         ax[q].imshow(dataset.pixel_array, cmap=plt.cm.bone)
-        if df.loc[file_path.split('/')[-1][:-4],1] != '-1':
-            mask = rle2mask(df.loc[file_path.split('/')[-1][:-4],1], 1024, 1024).T
+
+        #extracts image_id from the file path
+        image_id = file_path.split('\\')[-1].replace(".dcm", "")
+        print("Imageid: "+str(image_id))
+
+        #finds masks matching image_id
+        found_masks = df[df['ImageId'].str.match(image_id)]
+        found_masks = found_masks.values.tolist()
+
+
+        
+
+        #has multiple masks
+        if len(found_masks)>1:
+            print("Multiple masks")
+
+            # print(mask_data)
+
+            for x in range(0, len(found_masks)):
+                mask = rle2mask(found_masks[x][1], 1024, 1024).T
+
             ax[q].set_title('See Marker')
             ax[q].imshow(mask, alpha=0.3, cmap="Reds")
+
+        #has single mask
+        elif len(found_masks)==1 and found_masks[0][1] != '-1':
+            print("Single mask")
+
+            mask_data = found_masks[0][1]
+            # print("Mask data: "+str(mask_data))
+
+            mask = rle2mask(mask_data, 1024, 1024).T
+            ax[q].set_title('See Marker')
+            ax[q].imshow(mask, alpha=0.3, cmap="Reds")
+
+        #has no masks
         else:
+            print("Nothing to see")
             ax[q].set_title('Nothing to see')
+
+
+
+
+        print()
+
+    #displays everything
+    plt.show()
+
 
 
 # for file_path in glob.glob(images_path):
