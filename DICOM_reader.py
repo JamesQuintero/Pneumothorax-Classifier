f6c5df96f190e61c83ec3a0d1929d6e06e821b38
James Quintero <jquinte2@mail.sfsu.edu>
2019-09-16 12:57:39 -0700

    Added training DICOM data

--- DICOM_reader.py
+++ DICOM_reader.py
@@ -2,6 +2,9 @@ import glob #for loading DICOM files from disk
 import pydicom #for reading DICOM files
 import matplotlib.pyplot as plt #for displaying DICOM files
 
+
+images_path = './data/stage_2_images/*.dcm'
+
 def show_dcm_info(dataset):
     print("Filename.........:", file_path)
     print("Storage type.....:", dataset.SOPClassUID)
@@ -31,20 +34,48 @@ def plot_pixel_array(dataset, figsize=(10,10)):
     plt.show()
 
 
-# def plot_many_images():
-#     start = 5   # Starting index of images
-#     num_img = 4 # Total number of images to show
+#plots many of the DICOM images at once
+def plot_many_images():
+    start = 5   # Starting index of images
+    num_img = 4 # Total number of images to show
 
-#     fig, ax = plt.subplots(nrows=1, ncols=num_img, sharey=True, figsize=(num_img*10,10))
-#     for q, file_path in enumerate(glob.glob('../input/siim-acr-pneumothorax-segmentation/sample images/*.dcm')[start:start+num_img]):
-#         dataset = pydicom.dcmread(file_path)
-#         #show_dcm_info(dataset)
+    fig, ax = plt.subplots(nrows=1, ncols=num_img, sharey=True, figsize=(num_img*10,10))
+    dataset = glob.glob(images_path)
+
+    print("Len dataset: "+str(len(dataset)))
+
+    for q, file_path in enumerate(dataset[start:start+num_img]):
+
+        print(str(q)+": "+str(file_path))
+        dataset = pydicom.dcmread(file_path)
+        #show_dcm_info(dataset)
         
-#         ax[q].imshow(dataset.pixel_array, cmap=plt.cm.bone)
+        ax[q].imshow(dataset.pixel_array, cmap=plt.cm.bone)
 
 
-for file_path in glob.glob('./data/stage_2_images/*.dcm'):
-    dataset = pydicom.dcmread(file_path)
-    show_dcm_info(dataset)
-    plot_pixel_array(dataset)
-    # break # Comment this out to see all
+def display_masks():
+    df = pd.read_csv('./data/train_labels.csv', header=None, index_col=0)
+
+    print("Num training labels: "+str(df))
+
+    fig, ax = plt.subplots(nrows=1, ncols=num_img, sharey=True, figsize=(num_img*10,10))
+    for q, file_path in enumerate(glob.glob('../input/siim-acr-pneumothorax-segmentation/sample images/*.dcm')[start:start+num_img]):
+        dataset = pydicom.dcmread(file_path)
+        #print(file_path.split('/')[-1][:-4])
+        ax[q].imshow(dataset.pixel_array, cmap=plt.cm.bone)
+        if df.loc[file_path.split('/')[-1][:-4],1] != '-1':
+            mask = rle2mask(df.loc[file_path.split('/')[-1][:-4],1], 1024, 1024).T
+            ax[q].set_title('See Marker')
+            ax[q].imshow(mask, alpha=0.3, cmap="Reds")
+        else:
+            ax[q].set_title('Nothing to see')
+
+
+# for file_path in glob.glob(images_path):
+#     dataset = pydicom.dcmread(file_path)
+#     show_dcm_info(dataset)
+#     plot_pixel_array(dataset)
+#     # break # Comment this out to see all
+
+# plot_many_images()
+display_masks()
